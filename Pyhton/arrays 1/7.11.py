df151246aeb50183508c5a845564eff40a636e5d
Lazar-Djukovic <ldjukovic40@gmail.com>
2021-10-08 12:43:48 +0100

    cn
    changed rep name and 7. arrays

---
+++ Pyhton/arrays 1/7.11.py
@@ -0,0 +1,13 @@
+numbers = []
+counter = 0
+while counter < 6:
+ x = int(input())
+ numbers.append(x)
+ counter = counter + 1
+#endwhile
+ 
+ counter = 5
+ while counter >= 0:
+  print(numbers[counter])
+ counter = counter - 1
+#endwhile
---
+++ Pyhton/arrays 1/7.13.py
@@ -0,0 +1,10 @@
+o1s = [10,12,15,10]
+o2s = [5,8,3,6]
+o3s = [10,12,15,10]
+
+count = 0
+while count < 4:
+   ts = o1s[count] + o2s[count] + o3s[count]
+   print('Total for quarter ',count+1,'is', ts)
+   count = count + 1
+#endwhile
